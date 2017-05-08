65812422ab6627338d05d0739893561378245924
gmlove <gracekinglau@gmail.com>
2017-05-08 23:28:04 +0800

    builder script

--- builder.groovy
+++ builder.groovy
@@ -1,3 +1,43 @@
+class Stage implements Serializable {
+    String name
+
+    Stage(java.util.LinkedHashMap opts) {
+        name = opts.name
+    }
+}
+class BStage extends Stage {
+    String bp
+
+    BStage(java.util.LinkedHashMap opts) {
+        super(opts)
+        bp = opts.bp
+    }
+
+    def call1(env) {
+        env.stage('build') {
+            env.node('master') {
+                env.sh 'echo build - ' + bp
+            }
+        }
+    }
+}
+class DStage extends Stage {
+    String dp
+
+    DStage(java.util.LinkedHashMap opts) {
+        super(opts)
+        dp = opts.dp
+    }
+
+    def call1(env) {
+        env.stage('deploy - ' + dp) {
+            env.node('master') {
+                env.sh 'echo deploy - ' + dp
+            }
+        }
+    }
+}
+
 
 class Stages {
     def static stage(opts) {
@@ -5,46 +45,6 @@ class Stages {
             return new BStage(opts.options)
         } else if(opts.type == 'd') {
             return new DStage(opts.options)
-        }
-    }
-
-    static class Stage implements Serializable {
-        String name
-
-        Stage(java.util.LinkedHashMap opts) {
-            name = opts.name
-        }
-    }
-    static class BStage extends Stage {
-        String bp
-
-        BStage(java.util.LinkedHashMap opts) {
-            super(opts)
-            bp = opts.bp
-        }
-
-        def call1(env) {
-            env.stage('build') {
-                env.node('master') {
-                    env.sh 'echo build - ' + bp
-                }
-            }
-        }
-    }
-    static class DStage extends Stage {
-        String dp
-
-        DStage(java.util.LinkedHashMap opts) {
-            super(opts)
-            dp = opts.dp
-        }
-
-        def call1(env) {
-            env.stage('deploy - ' + dp) {
-                env.node('master') {
-                    env.sh 'echo deploy - ' + dp
-                }
-            }
         }
     }
 
